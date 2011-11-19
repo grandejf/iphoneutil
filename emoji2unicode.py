fb704d8c26a76875fd7c539e2df6a5872dcff796
grandejf <1028329+grandejf@users.noreply.github.com>
2011-11-18 21:32:56 -0600

    iOS 5 changes.  Using unicode in addition to softbank mappings.

--- emoji2unicode.py
+++ emoji2unicode.py
@@ -7,7 +7,7 @@ import xml.sax.handler
 
 class emoji_XMLHandler(xml.sax.handler.ContentHandler):
     def __init__(self):
-        self.encodings=["google","docomo","softbank","kddi"]
+        self.encodings=["google","docomo","softbank","kddi","unicode"]
         self.mappings={}
         for encoding in self.encodings:
             self.mappings[encoding]={}
@@ -22,7 +22,10 @@ class emoji_XMLHandler(xml.sax.handler.ContentHandler):
                     try:
                         map["unicode"]=unichr(int(attrs["unicode"],16))
                     except:
-                        map["unicode"]=None
+                        try:
+                            map["unicode"]=eval("u\"\\U"+str.rjust(str("%X"%int(attrs["unicode"],16)),8,"0")+"\"")
+                        except:
+                            map["unicode"]=None
                     pass
                 else:
                     map["unicode"]=None
@@ -37,7 +40,13 @@ class emoji_XMLHandler(xml.sax.handler.ContentHandler):
                         key=unichr(int(key,16))
                         pass
                     else:
-                        key=eval("u\"\\U"+str.rjust(str(key),8,"0")+"\"")
+                        if encoding=='unicode':
+                            try:
+                                key=eval("u\"\\U"+str.rjust(str("%X"%int(key,16)),8,"0")+"\"")
+                            except:
+                                continue
+                        else:
+                            key=eval("u\"\\U"+str.rjust(str(key),8,"0")+"\"")
                         pass
                     self.mappings[encoding][key]=map
                     pass
@@ -63,21 +72,36 @@ class Emoji:
         file.close()
         handler=emoji_XMLHandler()
         parseXML(handler,emojixml)
-        self.mappings=handler.mappings["softbank"]
+        self.mappings=handler.mappings
         pass
     def translate(self,input):
         output=[]
-        for i in xrange(len(input)):
-            char=input[i]
-            if self.mappings.has_key(char):
-                if self.mappings[char]["unicode"]!=None:
-                    output.append(self.mappings[char]["unicode"])
-                    output.append("[ALT="+self.mappings[char]["name"]+"]")
-                else:
-                    output.append("["+self.mappings[char]["name"]+"]")
-                    pass
+        import codecs
+        bytestr, _ = codecs.getencoder("utf_32_be")(input)
+        for i in xrange(0,len(bytestr),4):
+            code = 0
+            for b in bytestr[i:i + 4]:
+                code = (code << 8) + ord(b)
                 pass
-            else:
+            code = "%X" % code
+            char=eval("u\"\\U"+str.rjust(str(code),8,"0")+"\"")
+            foundIt=0
+            for encoding in ['softbank','unicode']:
+                map=self.mappings[encoding]
+                if map.has_key(char):
+                    if map[char]["unicode"]!=None:
+                        output.append(map[char]["unicode"])
+                        output.append(" [ALT="+map[char]["name"]+"]")
+                    else:
+                        output.append(" ["+map[char]["name"]+"]")
+                        pass
+                    foundIt=1
+                    break
+                pass
+            if not foundIt:
+                if (char==u'\xa0'):
+                    char=' '
+                    pass
                 output.append(char)
                 pass
             pass
