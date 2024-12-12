720e74eca66f967b2e1e4f44ee184589d2eb498b
grandejf <1028329+grandejf@users.noreply.github.com>
2024-12-11 21:53:48 -0600

    extracting guid for genmojis

--- abody2txt.swift
+++ abody2txt.swift
@@ -1,4 +1,16 @@
 import Foundation
+
+import func Darwin.fputs
+import var Darwin.stderr
+
+
+struct StderrOutputStream: TextOutputStream {
+  mutating func write(_ string: String) {
+    fputs(string, stderr)
+  }
+}
+var stdErr = StderrOutputStream()
+
 let filename=CommandLine.arguments[1]
 
 getAttributedBody(filename);
@@ -7,8 +19,33 @@ func getAttributedBody(_ filename:String) {
   var possibleObject:NSAttributedString
   if let data = NSData(contentsOfFile: filename) {
     possibleObject = NSUnarchiver.unarchiveObject(with:data as Data) as! NSAttributedString;
-    print(possibleObject.string)
+    // print(possibleObject, to: &stdErr)
+    // print(possibleObject.string, to: &stdErr)
+    
+    let text = possibleObject.string
+    var outtext = ""
+    
+    possibleObject.enumerateAttributes(in: NSRange(0..<possibleObject.length)) {
+      dict, range, stop in
+      let srange = Range(range, in:text)
+      var stext = text[srange as! Range]
+      if (false) {
+        for (key, value) in dict {
+          print(key.rawValue, to: &stdErr)
+          print(value,to: &stdErr)
+        }
+      }
+      if (dict[NSAttributedString.Key("__kIMEmojiImageAttributeName")] != nil) {
+        let guid = dict[NSAttributedString.Key("__kIMFileTransferGUIDAttributeName")]
+        if (guid != nil) {
+          stext = "[emoji_guid="+(guid as! String)+"]"
+        }
+      }
+      outtext += stext     
+    }
+    print(outtext)
   }
 }
+
 
 
